646bfe20f54d6eaa89233489e27fb32f2c766a3b
yy <yy>
2022-07-05 23:38:05 +0300

    init

--- Зачетное задание/Зачетное задание/DoubleLinkedNode/task.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedNode/task.py
@@ -1,6 +1,18 @@
 class Node:
     """ Класс, который описывает узел связного списка. """
-    ...
+    def __init__(self, value: Any):
+        """
+        Инициализация атрибутов класса
+        :param value:
+        """
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+
+
+    def __repr__(self):
+        ...
 
 
 class DoubleLinkedNode(Node):
--- Основы ООП/Лабораторные задания/task1_Node__init__repr__/main.py
+++ Основы ООП/Лабораторные задания/task1_Node__init__repr__/main.py
@@ -4,7 +4,7 @@ from typing import Any, Optional
 class Node:
     """ Класс, который описывает узел связного списка. """
 
-    def __init__(self, value: Any, next_: Optional["Node"] = None):
+    def __init__(self, value: Any):
         """
         Создаем новый узел для односвязного списка
         :param value: Любое значение, которое помещено в узел
@@ -12,15 +12,15 @@ class Node:
         """
         # TODO  инициализировать атрибуты экземпляра класса Node
         self.value = value
-        self.next_ = next_
+        self.next = None
 
     # TODO реализовать метод __repr__ для отображения экземпляра
     def __repr__(self) -> str:
-        return f"Node({self.value}, {self.next_})"
+        return f"Node({self.value}, {self.next})"
 
 
 if __name__ == '__main__':
-    first_node = Node(1) # TODO инициализировать первый узел
+    first_node = Node(1)  # TODO инициализировать первый узел
 
     second_node = Node(2)  # TODO инициализировать второй узел
     first_node.next = second_node  # TODO через атрибут экземпляра устанавливаем первому узлу следующий узел
